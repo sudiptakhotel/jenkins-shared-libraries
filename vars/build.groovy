e96ce94e8c611841649865f510d4da7d523870b6
sudipta khotel <121477770+sudiptakhotel@users.noreply.github.com>
2026-01-11 11:11:27 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
-def call(String imageName , String imageTag) {
+def call(String dockerHubUser , String imageName , String imageTag) {
     echo "Building the docker image"
-    sh "docker build -t ${imageName}:${imageTag} ."
+    sh "docker build -t ${dockerHubUser}/${imageName}:${imageTag} ."
 }
